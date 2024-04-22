2eda5707ad4b587d51bbc273e12d915a0413ea31
radrybinski <rybinski.radoslaw@gmail.com>
2024-04-22 11:06:13 +0200

    First lines of code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
+Console.WriteLine("Input number from 1 to 100");
+double numb1;
+string input1;
+input1 = Console.ReadLine();
+numb1 = Convert.ToDouble(input1);
+Console.WriteLine($"Your number is: {numb1}");
